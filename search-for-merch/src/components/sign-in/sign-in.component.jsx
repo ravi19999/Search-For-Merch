b653d69639e5a1abb7e8bf70a553de4358a7060a
ravi19999 <dahalravindra@gmail.com>
2020-10-21 19:28:09 +0545

    Styled the sign in component :lipstick: :sparkles:

--- search-for-merch/src/components/sign-in/sign-in.component.jsx
+++ search-for-merch/src/components/sign-in/sign-in.component.jsx
@@ -1,5 +1,7 @@
 import React from "react";
 
+import FormInput from "../form-input/form-input.component";
+
 import "./sign-in.styles.scss";
 
 class SignIn extends React.Component {
@@ -24,27 +26,27 @@ class SignIn extends React.Component {
 
   render() {
     return (
-      <div className="signin">
+      <div className="sign-in">
         <h2>I already have an account</h2>
         <span>Sign in with your email and password</span>
 
         <form onSubmit={this.handleSubmit}>
-          <input
+          <FormInput
             name="email"
             type="email"
             value={this.state.email}
-            onChange={this.handleChange}
+            handleChange={this.handleChange}
+            label="email"
             required
           />
-          <label>Email</label>
-          <input
+          <FormInput
             name="password"
             type="password"
             value={this.state.password}
-            onchange={this.handleChange}
+            handleChange={this.handleChange}
+            label="password"
             required
           />
-          <label>Password</label>
 
           <input type="submit" value="Submit Form" />
         </form>
