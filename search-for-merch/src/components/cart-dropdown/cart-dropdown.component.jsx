629e4192be4bac27f1473f779e3c099cb3b8dc66
ravi19999 <dahalravindra@gmail.com>
2020-11-05 19:44:33 +0545

    Toggled the cart dropdown component on and off on click :boom:

--- search-for-merch/src/components/cart-dropdown/cart-dropdown.component.jsx
+++ search-for-merch/src/components/cart-dropdown/cart-dropdown.component.jsx
@@ -8,7 +8,7 @@ const CartDropdown = () => (
   <div className="cart-dropdown">
     <div className="cart-items" />
 
-    <CustomButton>GO TO CHECKOUT </CustomButton>
+    <CustomButton>GO TO CHECKOUT</CustomButton>
   </div>
 );
 
