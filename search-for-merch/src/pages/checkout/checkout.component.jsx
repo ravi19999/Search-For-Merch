d62bbb84cd465be3044a89c15beddeefc9a7df93
ravi19999 <dahalravindra@gmail.com>
2020-11-09 20:40:58 +0545

    Populated the checkout page with product details and total :bento:

--- search-for-merch/src/pages/checkout/checkout.component.jsx
+++ search-for-merch/src/pages/checkout/checkout.component.jsx
@@ -1,7 +1,43 @@
 import React from "react";
 
+import { connect } from "react-redux";
+
+import { createStructuredSelector } from "reselect";
+
+import { selectCartItems } from "../../redux/cart/cart.selectors";
+import { selectCartTotal } from "../../redux/cart/cart.selectors";
+
 import "./checkout.styles.scss";
 
-const CheckoutPage = () => <div>CHECKOUT PAGE</div>;
+const CheckoutPage = ({ cartItems, total }) => (
+  <div className="checkout-page">
+    <div className="checkout-header">
+      <div classname="header-block">
+        <span>Product</span>
+      </div>
+      <div classname="header-block">
+        <span>Description</span>
+      </div>
+      <div classname="header-block">
+        <span>Quantity</span>
+      </div>
 
-export default CheckoutPage;
+      <div classname="header-block">
+        <span>Price</span>
+      </div>
+      <div classname="header-block">
+        <span>Remove</span>
+      </div>
+    </div>
+    {cartItems.map((cartItem) => cartItem.name)}
+    <div className="total">
+      <span>TOTAL:${total}</span>
+    </div>
+  </div>
+);
+const mapStateToProps = createStructuredSelector({
+  cartItems: selectCartItems,
+  total: selectCartTotal,
+});
+
+export default connect(mapStateToProps)(CheckoutPage);
