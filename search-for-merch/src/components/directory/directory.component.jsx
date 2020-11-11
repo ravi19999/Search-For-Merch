7b5aa43539c9e2d99d1785bb4d7959f140adbbc0
ravi19999 <dahalravindra@gmail.com>
2020-11-11 19:54:07 +0545

    Moved the shop state into redux :recycle:

--- search-for-merch/src/components/directory/directory.component.jsx
+++ search-for-merch/src/components/directory/directory.component.jsx
@@ -1,15 +1,14 @@
 import React from "react";
+import { connect } from "react-redux";
+import { createStructuredSelector } from "reselect";
+
+import { selectDirectorySections } from "../../redux/directory/directory.selectors";
 
 import MenuItem from "../menu-item/menu-item.component";
 
-import { connect } from "react-redux";
-
 import "./directory.styles.scss";
-import { createStructuredSelector } from "reselect";
 
-import { selectDirectorySections } from "../../redux/directory/directory.selector";
-
-const Directory = (sections) => (
+const Directory = ({ sections }) => (
   <div className="directory-menu">
     {sections.map(({ id, ...otherSectionProps }) => (
       <MenuItem key={id} {...otherSectionProps} />
