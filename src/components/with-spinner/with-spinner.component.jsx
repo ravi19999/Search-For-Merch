a1776746db9c4a064dbbc035a0e89c9c5fbf0ed3
ravi19999 <dahalravindra@gmail.com>
2020-12-07 17:41:52 +0545

    Created the with spinner HOC :boom:

---
+++ src/components/with-spinner/with-spinner.component.jsx
@@ -0,0 +1,17 @@
+import React from 'rect';
+
+import { SpinnerContainer, SpinnerOverlay } from './with-spinner.styles.jsx';
+
+const WithSpinner = WrappedComponent => {
+    ( {isLoading, ...otherProps} ) => {
+    return isLoading ? (
+        <SpinnerOverlay>
+            <SpinnerContainer/>
+        </SpinnerOverlay>
+    ) : (
+        <WrappedComponent {...otherProps} />
+        );
+    };
+};
+
+export default WithSpinner;
