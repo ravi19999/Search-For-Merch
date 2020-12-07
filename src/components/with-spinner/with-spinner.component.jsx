fc5d8915b732d586f54734942ccee1816d7cdc31
ravi19999 <dahalravindra@gmail.com>
2020-12-07 21:00:48 +0545

    Implemented spiner while fetching data from the database :boom:

--- src/components/with-spinner/with-spinner.component.jsx
+++ src/components/with-spinner/with-spinner.component.jsx
@@ -1,17 +1,18 @@
-import React from 'rect';
+import React from "react";
 
-import { SpinnerContainer, SpinnerOverlay } from './with-spinner.styles.jsx';
+import { SpinnerContainer, SpinnerOverlay } from "./with-spinner.styles";
 
-const WithSpinner = WrappedComponent => {
-    ( {isLoading, ...otherProps} ) => {
+const WithSpinner = (WrappedComponent) => {
+  const Spinner = ({ isLoading, ...otherProps }) => {
     return isLoading ? (
-        <SpinnerOverlay>
-            <SpinnerContainer/>
-        </SpinnerOverlay>
+      <SpinnerOverlay>
+        <SpinnerContainer />
+      </SpinnerOverlay>
     ) : (
-        <WrappedComponent {...otherProps} />
-        );
-    };
+      <WrappedComponent {...otherProps} />
+    );
+  };
+  return Spinner;
 };
 
 export default WithSpinner;
