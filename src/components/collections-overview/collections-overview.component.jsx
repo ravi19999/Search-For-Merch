48b198ae3d19ab681b86fb6f7d4d2f492984f542
ravi19999 <dahalravindra@gmail.com>
2020-12-28 15:31:54 +0545

    Replaced redux-thunk with saga :sparkles:

--- src/components/collections-overview/collections-overview.component.jsx
+++ src/components/collections-overview/collections-overview.component.jsx
@@ -4,10 +4,7 @@ import { createStructuredSelector } from "reselect";
 
 import CollectionPreview from "../collection-preview/collection-preview.component";
 
-import {
-    selectCollectionsForPreview,
-    selectIsCollectionsLoaded,
-} from "../../redux/shop/shop.selectors";
+import { selectCollectionsForPreview } from "../../redux/shop/shop.selectors";
 import { CollectionsOverviewContainer } from "./collections-overview.styles";
 
 const CollectionsOverview = ({ collections }) => (
