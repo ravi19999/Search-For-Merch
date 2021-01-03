80fae9ca325182c68bae9bf3483d8f4ae8686f57
ravi19999 <dahalravindra@gmail.com>
2021-01-03 19:01:58 +0545

    Updated user actions :sparkles:

--- src/redux/user/user.actions.js
+++ src/redux/user/user.actions.js
@@ -1,6 +1,33 @@
-import { UserActionTypes } from './user.types';
+import { UserActionTypes } from "./user.types";
 
-export const setCurrentUser = user => ({
-  type: UserActionTypes.SET_CURRENT_USER,
-  payload: user
+export const setCurrentUser = (user) => ({
+    type: UserActionTypes.SET_CURRENT_USER,
+    payload: user,
+});
+
+export const googleSignStart = () => ({
+    type: UserActionTypes.GOOGLE_SIGN_IN_START,
+});
+
+export const googleSignInSuccess = (user) => ({
+    type: UserActionTypes.GOOGLE_SIGN_IN_SUCCESS,
+    payload: user,
+});
+
+export const googleSignInFailure = (error) => ({
+    type: UserActionTypes.GOOGLE_SIGN_IN_FAILURE,
+    payload: error,
+});
+export const emailSiginStart = (emailAndPassword) => ({
+    type: UserActionTypes.EMAIL_SIGN_IN_START,
+    payload: emailAndPassword,
+});
+
+export const emailSignInSuccess = () => ({
+    type: UserActionTypes.EMAIL_SIGN_IN_SUCCESS,
+});
+
+export const emailSignInFailure = (error) => ({
+    type: UserActionTypes.Email_SIGN_IN_FAILURE,
+    payload: error,
 });
--- src/redux/user/user.types.js
+++ src/redux/user/user.types.js
@@ -7,3 +7,30 @@ export const UserActionTypes = {
     EMAIL_SIGN_IN_SUCCESS: "EMAIL_SIGN_IN_SUCCESS",
     EMAIL_SIGN_IN_FAILURE: "EMAIL_SIGN_IN_FAILURE",
 };
+
+export const googleSignStart = () => ({
+    type: UserActionTypes.GOOGLE_SIGN_IN_START,
+});
+
+export const googleSignInSuccess = (user) => ({
+    type: UserActionTypes.GOOGLE_SIGN_IN_SUCCESS,
+    payload: user,
+});
+
+export const googleSignInFailure = (error) => ({
+    type: UserActionTypes.GOOGLE_SIGN_IN_FAILURE,
+    payload: error,
+});
+export const emailSiginStart = (emailAndPassword) => ({
+    type: UserActionTypes.EMAIL_SIGN_IN_START,
+    payload: emailAndPassword,
+});
+
+export const emailSignInSuccess = () => ({
+    type: UserActionTypes.EMAIL_SIGN_IN_SUCCESS,
+});
+
+export const emailSignInFailure = (error) => ({
+    type: UserActionTypes.Email_SIGN_IN_FAILURE,
+    payload: error,
+});
